73e1d10a778a590d85a8caae89a5e8716cd4d0da
rafalum <de.rafael@pm.me>
2023-10-20 12:07:35 -0700

    skeleton for contracts

---
+++ optimism/contracts.py
@@ -0,0 +1,48 @@
+from web3 import Web3
+
+
+class OptimismPortal():
+
+    def __init__():
+        pass
+
+    def deposit_transaction():
+        pass
+    
+    def prove_withdrawl_transaction():
+        pass
+
+    def finalize_withdrawl_transaction():
+        pass
+
+    def is_output_finalized():
+        pass
+
+class StandardBridge():
+    
+    def __init__():
+        pass
+
+    def bridge_eth():
+        pass
+    
+    def bridge_eth_to():
+        pass
+
+    def bridge_erc20():
+        pass
+
+    def bridge_erc20_to():
+        pass
+
+class CrossChainMessenger():
+
+    def __init__():
+        pass
+
+
+    def send_message():
+        pass
+    
+    def message_nonce():
+        pass
