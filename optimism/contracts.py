d62844c50f1194ec1537ad6fd077f633805874ae
rafalum <de.rafael@pm.me>
2023-10-21 16:51:23 -0700

    cross chain messenger contract

--- optimism/contracts.py
+++ optimism/contracts.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 from .addresses import *
-from .utils import get_provider, load_abi
+from .utils import get_provider, load_abi, determine_direction
 
 class OptimismPortal():
 
@@ -58,12 +58,42 @@ class StandardBridge():
 
 class CrossChainMessenger():
 
-    def __init__():
-        pass
+    def __init__(self, account, from_chain_id=1, to_chain_id=10, to_l2=True, provider=None):
+
+        l1_to_l2 = determine_direction(from_chain_id, to_chain_id)
+        l2 = not l1_to_l2
+
+        if provider is None:
+            self.provider = get_provider(l2=l2)
+        else:
+            self.provider = provider
+        
+        self.from_chain_id = from_chain_id
+        self.to_chain_id = to_chain_id
+
+        self.account = account
+
+        if l1_to_l2:
+            self.contract = self.provider.eth.contract(address=L1_CROSS_CHAIN_MESSENGER, abi=load_abi("L1_CROSS_MESSENGER"))
+        else:
+            self.contract = self.provider.eth.contract(address=L2_CROSS_CHAIN_MESSENGER, abi=load_abi("L2_CROSS_MESSENGER"))
 
 
-    def send_message():
-        pass
+    def send_message(self, target, message, min_gas_limit, value=None):
+
+        send_message_tx = self.contract.functions.sendMessage(target, message, min_gas_limit).build_transaction({
+            "from": self.account.address,
+            "gas": min_gas_limit,
+            "nonce": self.provider.eth.get_transaction_count(self.account.address),
+            "value": 0 if value is None else value
+        })
+
+        signed_txn = self.provider.eth.account.sign_transaction(send_message_tx, self.account.key)
+        txn_hash = self.provider.eth.send_raw_transaction(signed_txn.rawTransaction)
+        txn_receipt = self.provider.eth.wait_for_transaction_receipt(txn_hash)
+
+        return txn_hash, txn_receipt
     
-    def message_nonce():
-        pass
+    def message_nonce(self):
+
+        return self.contract.functions.messageNonce().call()
