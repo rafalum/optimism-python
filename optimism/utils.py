35a70f3b7812893265e634b6e64dda94c09906ff
rafalum <de.rafael@pm.me>
2023-10-23 10:46:43 -0700

    use web3's solidity_keccak instead of sha3

--- optimism/utils.py
+++ optimism/utils.py
@@ -43,26 +43,12 @@ def determine_direction(from_chain_id, to_chain_id):
     else:
         return False
     
-HASH_ZERO = "0x" + "0" * 64  # This represents the ethers.constants.HashZero in a hexadecimal format.
-
 def hash_message_hash(message_hash: str) -> str:
-    """
-    Utility for hashing a message hash. This computes the storage slot
-    where the message hash will be stored in state. HashZero is used
-    because the first mapping in the contract is used.
-
-    Args:
-        message_hash (str): Message hash to hash.
-
-    Returns:
-        str: Hash of the given message hash.
-    """
-
-    # Encoding the message hash and HASH_ZERO. 
-    # This is a basic representation; you may need to adjust depending on how your data is structured.
-    data = message_hash + HASH_ZERO
-
-    # Compute the keccak256 hash of the data
-    k = sha3.keccak_256()
-    k.update(data.encode())  # Assuming the input is a utf-8 encoded string.
-    return "0x" + k.hexdigest()
+    # Web3 provides utility functions similar to ethers
+    w3 = Web3()
+    
+    # These are equivalent constants in web3.py for ethers.constants.HashZero
+    HASH_ZERO = int('0x0000000000000000000000000000000000000000000000000000000000000000', 16)
+    
+    # Use solidityKeccak for both encoding and hashing
+    return w3.solidity_keccak(['bytes32', 'uint256'], [message_hash, HASH_ZERO]).hex()
