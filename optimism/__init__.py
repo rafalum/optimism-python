3a705730eea8b7a06ee3091e3d8ca76feec38836
rafalum <de.rafael@pm.me>
2023-10-25 00:48:12 -0700

    init file

---
+++ optimism/__init__.py
@@ -0,0 +1,3 @@
+from .contracts import OptimismPortal, StandardBridge, L2ToL1MessagePasser, L2OutputOracle, CrossChainMessenger
+
+__all__ = ["OptimismPortal", "StandardBridge", "CrossChainMessenger", "L2OutputOracle", "L2ToL1MessagePasser"]
