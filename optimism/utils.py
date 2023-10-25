7ec330656ddcfacfa78a50e017c549e3c9ae1047
rafalum <de.rafael@pm.me>
2023-10-25 00:51:22 -0700

    added to_low_level_message

--- optimism/utils.py
+++ optimism/utils.py
@@ -7,6 +7,8 @@ import numpy as np
 from web3 import Web3
 from dotenv import load_dotenv
 
+from .constants import MESSAGE_PASSED_ID
+
 load_dotenv()
 
 def get_env_variable(var_name):
@@ -52,3 +54,28 @@ def hash_message_hash(message_hash: str) -> str:
     
     # Use solidityKeccak for both encoding and hashing
     return w3.solidity_keccak(['bytes32', 'uint256'], [message_hash, HASH_ZERO]).hex()
+
+def log_to_address(log):
+
+    return Web3.to_checksum_address("0x" + log[-40:])
+
+def to_low_level_message(txn, txn_receipt):
+
+    logs = txn_receipt.logs
+
+    for log in logs:
+        if log.topics[0].hex() == MESSAGE_PASSED_ID:
+            message_passed_log = log
+            break
+
+    message_length = int(message_passed_log.data[128:160].hex(), 16)
+
+    return {
+        "messageNonce": int(message_passed_log.topics[1].hex(), 16),
+        "sender": log_to_address(message_passed_log.topics[2].hex()),
+        "target": log_to_address(message_passed_log.topics[3].hex()),
+        "value": int(message_passed_log.data[:32].hex(), 16),
+        "minGasLimit": int(message_passed_log.data[32:64].hex(), 16),
+        "message": message_passed_log.data[160:160 + message_length].hex(),
+        "withdrawlHash": message_passed_log.data[96:128].hex()
+    }
