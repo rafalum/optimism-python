aa74f22cfd1fc13a420222bef8cde8d65dafe80c
rafalum <de.rafael@pm.me>
2024-06-25 15:08:04 +0200

    DisputeGameFactory and FaultDisputeGame

--- optimism/__init__.py
+++ optimism/__init__.py
@@ -1,4 +1,4 @@
 from .cross_chain_messenger import CrossChainMessenger
-from .contracts import OptimismPortal, StandardBridge, L2ToL1MessagePasser, L2OutputOracle, CrossChainMessengerContract
+from .contracts import OptimismPortal, StandardBridge, L2ToL1MessagePasser, L2OutputOracle, DisputeGameFactory, FaultDisputeGame, CrossChainMessengerContract
 
-__all__ = ["CrossChainMessenger", "OptimismPortal", "StandardBridge", "CrossChainMessengerContract", "L2OutputOracle", "L2ToL1MessagePasser"]
+__all__ = ["CrossChainMessenger", "OptimismPortal", "StandardBridge", "CrossChainMessengerContract", "L2OutputOracle", "DisputeGameFactory", "FaultDisputeGame", "L2ToL1MessagePasser"]
